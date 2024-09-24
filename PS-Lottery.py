6d36fd662277fc713412ed702ca23499d4d12c2b
eytan1998 <eytan1998@gmail.com>
2024-09-24 18:51:02 +0300

    work

--- PS-Lottery.py
+++ PS-Lottery.py
@@ -25,7 +25,11 @@ def bikroft(matrix: numpy.array) -> list:
         raise ValueError("must be a square matrix")
     row_sums = np.sum(matrix, axis=1)
     col_sums = np.sum(matrix, axis=0)
-    if not np.allclose(row_sums, 1) or not np.allclose(col_sums, 1) or not np.all(matrix >= 0):
+    if (
+        not np.allclose(row_sums, 1)
+        or not np.allclose(col_sums, 1)
+        or not np.all(matrix >= 0)
+    ):
         print(matrix)
         raise ValueError("must be a bistochastic matrix")
 
@@ -34,11 +38,15 @@ def bikroft(matrix: numpy.array) -> list:
 
     while not np.allclose(matrix, M0):
         # Replace zeros with a large value
-        modified_matrix = np.where(matrix == 0, np.Inf, matrix)
+        modified_matrix = np.where(matrix == 0, np.inf, matrix)
         # Apply linear_sum_assignment to find the optimal assignment
         row_indices, col_indices = linear_sum_assignment(modified_matrix)
         # Find the edges corresponding to the matched pairs
-        edges = [(row, col) for row, col in zip(row_indices, col_indices) if matrix[row, col] != 0]
+        edges = [
+            (row, col)
+            for row, col in zip(row_indices, col_indices)
+            if matrix[row, col] != 0
+        ]
 
         scalar = min(matrix[x][y] for x, y in edges)
 
@@ -90,17 +98,28 @@ def PS(agents, objects, preferences):
         # or how much time take to eat the object
         T.append([0] * num_objects)
         for o in MaxN:
-            T[stage + 1][o] = ((1 - np.sum(P[:, o])) /
-                               sum([1 for pref in preferences_tmp if len(pref) > 0 and pref[0] == o]))
+            T[stage + 1][o] = (1 - np.sum(P[:, o])) / sum(
+                [1 for pref in preferences_tmp if len(pref) > 0 and pref[0] == o]
+            )
         Tmin.append(min([o for i, o in enumerate(T[stage + 1]) if i in MaxN]))
 
         # Update P of the objects been eaten
         for a in agents:
             for o in objects:
-                if o in MaxN and len(preferences_tmp[a]) > 0 and preferences_tmp[a][0] == o:
+                if (
+                    o in MaxN
+                    and len(preferences_tmp[a]) > 0
+                    and preferences_tmp[a][0] == o
+                ):
                     P[a][o] = P[a][o] + Tmin[stage + 1]
         # Remove the eaten objects
-        O.append([o for o in O[stage] if o not in [x for x in MaxN if T[stage + 1][x] == Tmin[stage + 1]]])
+        O.append(
+            [
+                o
+                for o in O[stage]
+                if o not in [x for x in MaxN if T[stage + 1][x] == Tmin[stage + 1]]
+            ]
+        )
         stage += 1
     return P
 
@@ -109,7 +128,7 @@ def cut_capacity(G, S):
     capacity = 0
     for u, v in G.edges():
         if u in S and v not in S:
-            capacity += G[u][v]['capacity']
+            capacity += G[u][v]["capacity"]
     return capacity
 
 
@@ -132,21 +151,30 @@ def EPS(agents, objects, preferences):
     preferences_tmp = copy.deepcopy(preferences)
     preferences_tmp = [
         [set([pref]) if not isinstance(pref, set) else pref for pref in agent_pref]
-        if isinstance(agent_pref, list) else agent_pref for agent_pref in preferences_tmp]
+        if isinstance(agent_pref, list)
+        else agent_pref
+        for agent_pref in preferences_tmp
+    ]
     C = [[0] * n]
     H = [preferences_tmp[x - 1][0] for x in agents if len(preferences_tmp[x - 1]) > 0]
 
-    G.add_edges_from([(u, T, {'capacity': 1}) for u in A])
-    G.add_edges_from([(u, v, {'capacity': np.Infinity}) for u in agents for v in A if (v - n - 1) in H[u - 1]])
+    G.add_edges_from([(u, T, {"capacity": 1}) for u in A])
+    G.add_edges_from(
+        [
+            (u, v, {"capacity": np.inf})
+            for u in agents
+            for v in A
+            if (v - n - 1) in H[u - 1]
+        ]
+    )
 
     L_original = 1e-10
-    G.add_edges_from([(S, u, {'capacity': C[k][u - 1] + L_original}) for u in agents])
+    G.add_edges_from([(S, u, {"capacity": C[k][u - 1] + L_original}) for u in agents])
 
     while A:
-
         L = L_original  # (nx.cut_size(G, {T}, range(0, 10), weight='capacity')) / len(agents)-0.1
         for u in G.successors(S):
-            G[S][u]['capacity'] = C[k][u - 1] + L
+            G[S][u]["capacity"] = C[k][u - 1] + L
         value, cut = nx.minimum_cut(G, S, T)
         low = L
         high = 1 - 1e-10
@@ -154,7 +182,7 @@ def EPS(agents, objects, preferences):
             mid = (low + high) / 2
             G_TMP = G
             for u in G_TMP.successors(S):
-                G_TMP[S][u]['capacity'] = C[k][u - 1] + mid
+                G_TMP[S][u]["capacity"] = C[k][u - 1] + mid
             value, cut = nx.minimum_cut(G_TMP, S, T)
             if cut[0] == {0}:
                 low = mid
@@ -169,7 +197,7 @@ def EPS(agents, objects, preferences):
 
         L = low
         for u in G.successors(0):
-            G[0][u]['capacity'] = C[k][u - 1] + L
+            G[0][u]["capacity"] = C[k][u - 1] + L
         flow, flow_dict = nx.maximum_flow(G, S, T)
         for x in agents:
             if x not in cut[0]:
@@ -192,12 +220,22 @@ def EPS(agents, objects, preferences):
             for pref in agent_pref:
                 # if isinstance(pref, set):
                 pref -= objects_to_remove
-        preferences_tmp = [[pref for pref in agent_pref if pref] for agent_pref in preferences_tmp]
+        preferences_tmp = [
+            [pref for pref in agent_pref if pref] for agent_pref in preferences_tmp
+        ]
 
-        H = [preferences_tmp[x - 1][0] if len(preferences_tmp[x - 1]) > 0 else [] for x in agents]
+        H = [
+            preferences_tmp[x - 1][0] if len(preferences_tmp[x - 1]) > 0 else []
+            for x in agents
+        ]
         G.add_edges_from(
-            [(u, v, {'capacity': np.Infinity}) for u in agents for v in A if
-             v - n - 1 in H[u - 1]])
+            [
+                (u, v, {"capacity": np.inf})
+                for u in agents
+                for v in A
+                if v - n - 1 in H[u - 1]
+            ]
+        )
 
         C.append([0 if x in in_cut else C[k][x - 1] + L for x in agents])
         G.remove_nodes_from([a for a in agents if preferences_tmp[a - 1] == []])
@@ -240,7 +278,7 @@ def PS_Lottery(agents, objects, preferences, use_EPS=False):
     m = len(objects)
     c = math.ceil(m / n)
     # 1 make dummy object
-    dummy = list(range(n + 1, n + n * c - m + 1))
+    dummy = list(range(m, m + (n * c - m)))
     # 2 add the dummy to the real
     objects_dummy = objects + dummy
     # 4 new preference
@@ -249,7 +287,9 @@ def PS_Lottery(agents, objects, preferences, use_EPS=False):
     if use_EPS:
         P = EPS(agents, objects_dummy, preferences_dummy)
     else:
-        preferences_dummy = [break_lexicographically(pref) for pref in preferences_dummy]
+        preferences_dummy = [
+            break_lexicographically(pref) for pref in preferences_dummy
+        ]
         P = PS(agents, objects_dummy, preferences_dummy)
     preferences_dummy = [break_lexicographically(pref) for pref in preferences_dummy]
     # split to presenters
@@ -271,7 +311,9 @@ def PS_Lottery(agents, objects, preferences, use_EPS=False):
     result = []
     for item in bikroft(extP):
         remove_dummy = np.delete(item[1], dummy, axis=1)
-        stack_agents = np.array(np.sum([remove_dummy[row::n] for row in range(n)], axis=1))
+        stack_agents = np.array(
+            np.sum([remove_dummy[row::n] for row in range(n)], axis=1)
+        )
         result.append((item[0], stack_agents))
     return result
 
@@ -281,13 +323,13 @@ def print_array(array):
         print("    " + " ".join(str(int(cell)) for cell in row))
 
 
-def print_PS_Lottery(result):
-    print("=" * 40)
-    for item in result:
-        print(f"Probability: {item[0]}")
-        print("Matrix:")
-        print_array(item[1])
-        print()
+# def print_PS_Lottery(result):
+#     print("=" * 40)
+#     for item in result:
+#         print(f"Probability: {item[0]}")
+#         print("Matrix:")
+#         print_array(item[1])
+#         print()
 
 
 def measure_time(func, *args, **kwargs):
@@ -332,19 +374,224 @@ def compare_solution_methods():
         for _ in range(iteration):
             objects = np.random.randint(size - 5, size + 5)
             prefrence = rnd_pref(size, objects)
-            PS_times_for_average.append(measure_time(PS_Lottery, list(range(size)), list(range(objects)), prefrence,use_EPS=False))
-            EPS_times_for_average.append(measure_time(PS_Lottery, list(range(size)), list(range(objects)), prefrence,use_EPS=True))
+            PS_times_for_average.append(
+                measure_time(
+                    PS_Lottery,
+                    list(range(size)),
+                    list(range(objects)),
+                    prefrence,
+                    use_EPS=False,
+                )
+            )
+            EPS_times_for_average.append(
+                measure_time(
+                    PS_Lottery,
+                    list(range(size)),
+                    list(range(objects)),
+                    prefrence,
+                    use_EPS=True,
+                )
+            )
 
         PS_times.append(np.average(PS_times_for_average))
         EPS_times.append(np.average(EPS_times_for_average))
-    plt.plot(sizes, PS_times, 'g-', label='PS')
-    plt.plot(sizes, EPS_times, 'b-', label='EPS')
-    plt.xlabel('Size')
-    plt.ylabel('time in sec')
+    plt.plot(sizes, PS_times, "g-", label="PS")
+    plt.plot(sizes, EPS_times, "b-", label="EPS")
+    plt.xlabel("Size")
+    plt.ylabel("time in sec")
     plt.legend()
-    plt.savefig("compere.png")  # after you plot the graphs, save them to a file and upload it separately.
+    plt.savefig(
+        "compere.png"
+    )  # after you plot the graphs, save them to a file and upload it separately.
     plt.show()  # this should show the plot on your screen
 
+def combine_columns(matrix, n):
+    # Convert to a NumPy array if it's not already
+    matrix = np.array(matrix)
 
-if __name__ == '__main__':
-    compare_solution_methods()
+    # Calculate the number of new columns
+    num_rows, num_cols = matrix.shape
+    new_cols_count =  n  # ceil(num_cols / n)
+
+    # Initialize an empty array for the combined columns
+    combined_matrix = np.zeros((num_rows, new_cols_count))
+
+    # Combine columns
+    for i in range(num_cols):
+        col_index = i % n
+        combined_matrix[:, col_index] += matrix[:, i]
+
+    return combined_matrix
+
+def print_mat(matrix, row, col):
+    row_names = list(row)
+    column_names = list(col)
+
+    matrix = combine_columns(matrix=matrix,n= len(col))
+
+    # Ensure the length of row_names and column_names match the dimensions of the matrix
+    assert (
+        len(row_names) == matrix.shape[0]
+    ), "Row names length must match number of rows in the matrix."
+    assert (
+        len(column_names) == matrix.shape[1]
+    ), "Column names length must match number of columns in the matrix."
+
+    # Print table header
+    header = " | ".join(f"{col:10}" for col in column_names)
+    print(f"{'':10} | {header}")
+    print("-" * (len(header) + 12))  # Adjust based on column width
+
+    # Print table rows
+    for row_name, row_data in zip(row_names, matrix):
+        row_str = "\t".join(f"{item:10}" for item in row_data)
+        print(f"{row_name:10} | {row_str}")
+
+
+def isEF(mat, pref) -> bool:
+    for me in range(len(mat)):
+        for other in range(len(mat)):
+            if me == other:
+                continue
+            my_sum = 0
+            is_sum = 0
+            for item in pref[me]:
+                my_sum += sum(
+                    [
+                        mat[me][theItem]
+                        for theItem in range(item, len(mat[0]), len(pref) + 1)
+                    ]
+                )
+                is_sum += sum(
+                    [
+                        mat[other][theItem]
+                        for theItem in range(item, len(mat[0]), len(pref) + 1)
+                    ]
+                )
+                if is_sum - 0.00001 > my_sum:
+                    return False
+    return True
+
+
+def isEF1(mat, pref) -> bool:
+    for me in range(len(mat)):
+        for other in range(len(mat)):
+            if me == other:
+                continue
+            my_sum = 0
+            is_sum = -1
+            for item in pref[me]:
+                my_sum += sum(
+                    [
+                        mat[me][theItem]
+                        for theItem in range(item, len(mat[0]), len(pref) + 1)
+                    ]
+                )
+                is_sum += sum(
+                    [
+                        mat[other][theItem]
+                        for theItem in range(item, len(mat[0]), len(pref) + 1)
+                    ]
+                )
+                if is_sum - 0.00000000001 > my_sum:
+                    raise ValueError("The denominator cannot be zero.")
+                    return (is_sum,my_sum)
+    return True
+
+
+def print_PS_Lottery(result, r, c, prefe):
+    print("=" * 40)
+    index = 0
+    for item in result:
+        print(f"Probability: {item[0]}")
+        print(f"Matrix No {index}:")
+        print_mat(item[1], r, c)
+        print(f"Is ex-post EF? {isEF(item[1],prefe)}")
+        print(
+            f'Is ex-post EF-1? {True if isEF1(item[1],prefe) else "=========================================="}'
+        )
+        print()
+        index+=1
+
+
+def update(originalPref, allocaion, day, leap=1):
+    modul = len(originalPref[0])
+    newL = [[l[i] + (modul * day) for i in range(len(l))] for l in originalPref]
+    newLL = []
+    for i in range(len(newL)):
+        leap = 0
+        tmp = []
+        first = newL[i][0]
+        allocation_befor_first = [x for x in allocaion[i] if first % modul == x % modul]
+        tmp += allocation_befor_first
+        tmp += [first]
+        tmp += [x for x in allocaion[i] if x not in allocation_befor_first]
+        tmp += [x for x in newL[i] if x != first]
+        newLL.append(tmp)
+    return newLL
+
+
+if __name__ == "__main__":
+    # compare_solution_methods()
+    day = 0
+    agent = [0, 1, 2]
+    item = [0, 1, 2, 3]
+    alloction = [[] for _ in agent]
+    originalPref = [[0, 1, 2, 3], [0, 2, 1, 3], [3, 1, 0, 2]]
+    while True:
+        print(f'=======================================\n                   day {day}              \n===============================')
+        thisDayItem = list(range((day+1)*len(item)))
+        pred = update(originalPref=originalPref, allocaion=alloction, day=day)
+        
+
+        ps_mat = PS(agent, thisDayItem, pred)
+        print_mat(ps_mat, row=agent, col=item)
+        print(f"Is ex-ante EF? {isEF(ps_mat,pred)}")
+        print(
+            f'Is ex-ante EF-1? {True if isEF1(ps_mat,pred) else "=========================================="}'
+        )
+
+        result = PS_Lottery(agent, thisDayItem, pred)
+        print_PS_Lottery(result=result, r=agent, c=item, prefe=originalPref)
+
+        print("What you like to do? \nq) quit the program \n0-inf) enter the number of the matrix you want to allocate(default)")
+        user_input = input()
+
+        if user_input == "q":
+            break
+        if int(user_input) >= len(result):
+            break
+        # update allocation
+        # get last 4 item
+        mat = result[int(user_input)][1]
+        newAllc = []
+        for ag in agent:
+            newAgPref = []
+            extendPref = [x + day * len(item) for x in originalPref[ag]]
+            for it in extendPref:
+                newAgPref += [x for x in alloction[ag] if x != it and x % len(item) == it % len(item)]
+                if mat[ag][it] == 0:
+                    continue
+                # all the item the was prev to this
+                newAgPref.append(it)
+                # add the item to alloc in order
+            alloction[ag] = newAgPref
+        day += 1
+
+
+# it = [0,1,2,3]
+# pred = [[0,1,2,3], [0, 2,1,3], [3,1,0,2]]
+# [[1],[0,2],[3]]
+# =============
+# it = [0,1,2,3,4,5,6,7]
+# pred = [[4, 1, 5, 6, 7], [0, 4, 2, 6, 5, 7], [3, 7, 5, 4, 6]]
+# [[4,1,6],[0,2],[3,7,5]]
+# =============
+# it = [0,1,2,3,4,5,6,7,8,9,10,11]
+# pred = [[4, 8, 1, 6, 9, 10, 11], [0, 8, 2, 10, 9, 11], [3, 7, 11, 5, 9, 8, 10]]
+# [[4,8,1,6],[0,9,2,10],[3,7,11,5]]
+# =============
+# it = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+# pred = [[4, 8, 12, 1, 6, 13, 14, 15], [0, 12, 9, 2, 10, 14, 13, 15], [3, 7, 11, 15, 5, 13, 12, 14]]
+# [[4,8,1,13,6],[0,12,9,2,10],[3,7,11,15,5,14]]
+# =============
